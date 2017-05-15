2c18afe749417d7fff65ba6bbe04478a6f3ea756
43p3n0k <43p3n0k@gmail.com>
2017-05-15 21:09:15 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,29 @@
+var http = require('http');
+var fs = require('fs');
+var server = new http.Server();
+var url = require('url');
+
+server.listen(80, '127.0.0.1');
+server.on('request', function(req,res){
+    if(req.url!="/"){
+        fs.readFile("."+req.url,function(err, html){
+            if(err){
+                res.statusCode = 404;
+                res.end("Page not found");
+                return;
+            }
+            //res.writeHeader(200, {"Content-Type": "text/html"});
+            res.write(html);
+            res.end();
+        });
+    }else{
+        fs.readFile("./Tadpoles.html",function(err, html){
+            if(err){
+                return;
+            }
+            //res.writeHeader(200, {"Content-Type": "text/html"});
+            res.write(html);
+            res.end();
+        });
+    }
+});
